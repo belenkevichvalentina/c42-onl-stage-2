26338d74c2b0013c90a0bbcdd0eb786d0f392760
Vala <elfhanter85@gmail.com>
2026-09-17 01:56:35 +0300

    lesson homework 27

--- src/main/java/com/example/c42onl/homework_25/DownloadBookServlet.java
+++ src/main/java/com/example/c42onl/homework_25/DownloadBookServlet.java
@@ -6,7 +6,10 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 // GET http://localhost:8080/load-book
 @WebServlet(name = "DownloadBookServlet", value = "/load-book")
@@ -15,5 +18,23 @@ public class DownloadBookServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("DownloadBookServlet Servlet");
         resp.getWriter().println("load-book DownloadBookServlet");
+//        File bookFile = new File("D:\\java_book.txt");
+//        if (!bookFile.exists()) {
+//            resp.setContentType("text/html;charset=UTF-8");
+//            resp.getWriter().println("<h3> Ошибка: Книга на сервере не найдена.</h3>");
+//            return;
+//        }
+//        resp.setContentType("application/octet-stream");
+//        resp.setContentLength((int) bookFile.length());
+//        resp.setHeader("Content-Disposition", "attachment; filename=\"" + bookFile.getName() + "\"");
+//        try(FileInputStream inputStream = new FileInputStream(bookFile);
+//            OutputStream outputStream = resp.getOutputStream()) {
+//            byte[] buffer = new byte[4096];
+//            int bytesRead;
+//            while ((bytesRead = inputStream.read(buffer)) != -1){
+//                outputStream.write(buffer, 0, bytesRead);
+//            }
+//            outputStream.flush();
+//        }
     }
 }
--- src/main/java/com/example/c42onl/homework_25/UploadBookServlet.java
+++ src/main/java/com/example/c42onl/homework_25/UploadBookServlet.java
@@ -1,12 +1,16 @@
 package com.example.c42onl.homework_25;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /*Написать приложение, которое при запуске будет сообщать в консоль что оно
 работает. На любой запрос в консоли должна отображаться запись со временем этого
@@ -16,12 +20,47 @@ import java.io.IOException;
 /load-book - который позволит загружать свои книги на сервер.*/
 
 @WebServlet(name = "UploadBookServlet", value = "/book")
-
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2,
+        maxFileSize = 1024 * 1024 * 50,
+        maxRequestSize = 1024 * 1024 * 100
+)
 public class UploadBookServlet extends HttpServlet {
     @Override  // GET http://localhost:8080/book
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("UploadBookServlet Servlet");
         resp.getWriter().println("book UploadBookServlet");
-
+//        resp.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter writer = resp.getWriter()) {
+//            writer.println("<h3> Загрузка своей книги на сервер</h3>");
+//            writer.println("<form action='load-book' method='post' enctype='multipart/form-data'>");
+//            writer.println("<input type='file' name='bookFile' required><br><br>");
+//            writer.println("<button type='submit'>Отправить книгу на сервер</button>");
+//            writer.println("</form>");
+//        }
+//    }
+//
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.setContentType("text/html;charset=UTF-8");
+//        PrintWriter writer = resp.getWriter();
+//        String uploadPath = "D:\\uploaded_books";
+//        File uploadDir = new File(uploadPath);
+//        if (!uploadDir.exists()) {
+//            uploadDir.mkdir();
+//        }
+//        try{
+//            Part filePart = req.getPart("bookFile");
+//            String fileName = filePart.getSubmittedFileName();
+//            String finalPath = uploadPath + File.separator + fileName;
+//            writer.println("<h3 style='color:green;'> Книга '" + fileName + "' успешно загружена на сервер!</h3>");
+//            writer.println("<p>Путь сохранения: <i>" + finalPath + "</i></p>");
+//            writer.println("<a href='load-book'>Загрузить еще одну</a>");
+//        }catch (Exception e) {
+//            writer.println("<h3 style='color:red;'> Ошибка загрузки файла: " + e.getMessage() + "</h3>");
+//        }
     }
+
 }
+
